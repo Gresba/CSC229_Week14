95f230eaaec92e8c53dd4bb18527c8e5c20e07cd
kimpaul0709@gmail.com <>
2021-12-03 17:43:56 -0500

    Week 14

--- CSC229_Nov_08/src/main/java/Problem02.java
+++ CSC229_Nov_08/src/main/java/Problem02.java
@@ -5,10 +5,9 @@ public class Problem02 {
     public static void main(String[] args){
         Stack<String> ops = new Stack<String>();
         Stack<Double> values = new Stack<Double>();
-       
-     // Scanner StdIn = new Scanner("( ( 1 + 5 ) - ( 4 + 7 ) )"); // Run: -5.0 
-        Scanner StdIn = new Scanner("( 1 + ( ( 2 - 3 ) * ( 14 * 5 ) ) )"); // Run -69.0
-     // Scanner StdIn = new Scanner("( ( 1 + sqrt ( 5 ) ) / 2.0 )"); // Run 1.618033988749895
+
+        Scanner StdIn = new Scanner("( 1 + ( ( 2 - 3 ) * ( 14 * 5 ) ) )"); 
+        Scanner StdIn1 = new Scanner("( ( 1 + sqrt ( 5 ) ) / 2.0 )"); 
         
         while (StdIn.hasNext()){
             String s = StdIn.next();
@@ -19,8 +18,8 @@ public class Problem02 {
             else if (s.equals("/"))       ops.push(s);
             else if (s.equals("sqrt"))    ops.push(s);
             else if (s.equals(")")){
-                String op = ops.pop();
-                double v = values.pop();
+            String op = ops.pop();
+            double v = values.pop();
                 
                 if (op.equals("+"))       v = values.pop() + v;
                 if (op.equals("-"))       v = values.pop() - v;
